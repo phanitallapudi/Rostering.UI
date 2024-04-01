16754a9b044dd2849588bddd18a503d1fd4105cd
Jayant Verma <iamvermajayant@gmail.com>
2024-04-01 13:42:44 +0530

    ⚙ fixed chat bot z index

--- src/partials/customer/AssignedMap.jsx
+++ src/partials/customer/AssignedMap.jsx
@@ -10,27 +10,27 @@ const AssignedMap = ({ routePoints, ticket }) => {
   console.log(ticket);
 
   return (
-    <MapContainer center={position} zoom={15} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Polyline positions={routePoints.map(point => [point.latitude, point.longitude])} color="blue" />
-      <Marker position={[source.latitude, source.longitude]}>
-        <Popup>
-          {ticket.assigned_to.name}
-          <br></br>
-          {ticket.assigned_to.address}
-          <br></br>
-          {ticket.assigned_to.phoneno}
-        </Popup>
-      </Marker>
-      <Marker position={[destination.latitude, destination.longitude]}>
-        <Popup>
-          Destination
-        </Popup>
-      </Marker>
-    </MapContainer>
+      <MapContainer center={position} zoom={15} style={{ height: "100vh", width: "100%"}}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Polyline positions={routePoints.map(point => [point.latitude, point.longitude])} color="blue" />
+        <Marker position={[source.latitude, source.longitude]}>
+          <Popup>
+            {ticket.assigned_to.name}
+            <br></br>
+            {ticket.assigned_to.address}
+            <br></br>
+            {ticket.assigned_to.phoneno}
+          </Popup>
+        </Marker>
+        <Marker position={[destination.latitude, destination.longitude]}>
+          <Popup>
+            Destination
+          </Popup>
+        </Marker>
+      </MapContainer>
   );
 };
 
